14bd098ab32a7ddf44360efb21e9a71777c38d27
Hamiltondan93 <hamiltondan93@gmail.com>
2023-02-20 15:14:17 +0000

    Add: equals and to string

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 @Entity
 public class Author {
@@ -50,6 +51,32 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
 
